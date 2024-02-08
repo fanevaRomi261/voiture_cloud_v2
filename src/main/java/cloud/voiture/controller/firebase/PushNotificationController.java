2f562a481e54eb9c9046b3dcb52dc7a3c9f3ccd5
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2024-02-08 18:19:46 +0300

    Update PushNotificationController.java

--- src/main/java/cloud/voiture/controller/firebase/PushNotificationController.java
+++ src/main/java/cloud/voiture/controller/firebase/PushNotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import cloud.voiture.model.firebase.Token;
 import cloud.voiture.model.response.ErrorRes;
 import cloud.voiture.service.AnnonceService;
